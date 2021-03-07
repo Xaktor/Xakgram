144d95268d4de991b155850273178f76c95f0f07
Xaktor <71800595+Xaktor@users.noreply.github.com>
2021-03-07 15:12:02 -0500

    Xakgram 1.1

--- app/src/main/java/com/example/xakgram/LoginActivity.java
+++ app/src/main/java/com/example/xakgram/LoginActivity.java
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     final private String TAG= "LoginActivity";
     private EditText etUsername;
     private EditText etPassword;
-    private Button btnLogin;
+    private Button btnLogin,btnSignup;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,14 +35,21 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.username);
         etPassword = findViewById(R.id.password);
         btnLogin=findViewById(R.id.btnLogin);
+        btnSignup=findViewById(R.id.btnSignup);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG,"onClick login button");
                 String username=etUsername.getText().toString();
                 String password=etPassword.getText().toString();
-                Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT);
+                Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT).show();
                 loginUser(username,password);
+            }
+        });
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
             }
         });
         /*Log.i(TAG,"onClick login button");
@@ -51,7 +58,24 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT);
         loginUser(username,password);*/
     }
+    private void createUser() {
+        ParseUser user = new ParseUser();
+        user.setUsername(etUsername.getText().toString());
+        user.setPassword(etPassword.getText().toString());
+        // Other fields can be set just like any other ParseObject,
+        // using the "put" method, like this: user.put("attribute", "its value");
+        // If this field does not exists, it will be automatically created
 
+        user.signUpInBackground(e -> {
+            if (e == null) {
+                // Hooray! Let them use the app now.
+            } else {
+                // Sign up didn't succeed. Look at the ParseException
+                // to figure out what went wrong
+                Toast.makeText(LoginActivity.this, "You are ready to login", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     private void loginUser(String username, String password){
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
@@ -61,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 goMainActivity();
-                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT);
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -69,6 +93,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i =new Intent(this, MainActivity.class);
         startActivity(i);
-        finish();
+        //finish();
     }
 }
